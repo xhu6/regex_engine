a270e77024873a9804258f5c42e02ce78fce5206
xhu6 <143360206+xhu6@users.noreply.github.com>
2025-09-01 17:20:10 +0000

    Add benchmarks

---
+++ benches/benchmark.rs
@@ -0,0 +1,35 @@
+use std::hint::black_box;
+
+use criterion::{Criterion, criterion_group, criterion_main};
+
+const PATTERN: &str = "(a|b)*c+(b|a)*";
+const TEXT: &str = "abbabababababbabbbababababbaabbcabbbab";
+
+pub fn regex_engine_bench(c: &mut Criterion) {
+    use regex_engine::Regex;
+
+    c.bench_function("regex_engine new", |b| {
+        b.iter(|| Regex::new(black_box(PATTERN)))
+    });
+
+    let r = Regex::new(PATTERN);
+
+    c.bench_function("regex_engine check", |b| {
+        b.iter(|| r.check(black_box(TEXT)))
+    });
+}
+
+pub fn regex_crate_bench(c: &mut Criterion) {
+    use regex::Regex;
+
+    c.bench_function("regex new", |b| b.iter(|| Regex::new(black_box(PATTERN))));
+
+    // Full Match
+    let p = "^".to_string() + PATTERN + "$";
+    let r = Regex::new(&p).unwrap();
+
+    c.bench_function("regex is_match", |b| b.iter(|| r.is_match(black_box(TEXT))));
+}
+
+criterion_group!(benches, regex_engine_bench, regex_crate_bench);
+criterion_main!(benches);
